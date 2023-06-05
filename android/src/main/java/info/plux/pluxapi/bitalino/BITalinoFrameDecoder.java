a2714a6727a0e1b8af50e78f594f78c7568dca9d
MatGendron <gendronmathis@gmail.com>
2023-06-05 10:23:25 +0200

    Changed function to get hex string from byte array

--- android/src/main/java/info/plux/pluxapi/bitalino/BITalinoFrameDecoder.java
+++ android/src/main/java/info/plux/pluxapi/bitalino/BITalinoFrameDecoder.java
@@ -12,7 +12,7 @@ package info.plux.pluxapi.bitalino;
 import android.annotation.TargetApi;
 import android.util.Log;
 import android.os.Build;
-import org.apache.commons.codec.binary.Hex;
+import java.nio.charset.StandardCharsets
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -25,7 +25,7 @@ public class BITalinoFrameDecoder {
 
         try {
             Log.e(TAG, Arrays.toString(buffer));
-            Log.e(TAG, Hex.encodeHexString(buffer));
+            Log.e(TAG, bytesToHex(buffer));
             BITalinoFrame frame;
             //RTC: -4 à toutes les valeurs d'index du buffer pour prendre en compte
             //les données de temps ajoutées
@@ -78,5 +78,16 @@ public class BITalinoFrameDecoder {
         } catch (Exception e) {
             throw new BITalinoException(BITalinoErrorTypes.DECODE_INVALID_DATA);
         }
+    }
+
+    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+    public static String bytesToHex(byte[] bytes) {
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars, StandardCharsets.UTF_8);
     }
 }
